bf76d0725ad693e92fc2a15bd6bad4aa6ee37035
sedhainr2004 <sedhainrohan@gmail.com>
2023-11-29 21:48:39 -0500

    Dummy data added

--- FoundIt/FoundIt/Models/Item.swift
+++ FoundIt/FoundIt/Models/Item.swift
@@ -8,9 +8,20 @@
 import Foundation
 
 struct Item: Codable {
+    let image: String
     let desc: String
     let locDesc: String
     let time: String
-    let status: String
+    let status: Bool
     let contact: String
+}
+
+extension Item {
+    
+    static let dummyItems = [
+        Item(image: "WaterBottle", desc: "Water Bottle", locDesc: "Olin Hall", time: "11-15-2023", status: true, contact: "rs2443@cornell.edu"),
+        Item(image: "Charger", desc: "Charger", locDesc: "Mann Hall", time: "11-15-2023", status: false, contact: "ad243@cornell.edu"),
+        Item(image: "Pencil", desc: "Pencil", locDesc: "Uris Hall", time: "11-15-2023", status: true, contact: "sj543@cornell.edu")
+    ]
+    
 }
