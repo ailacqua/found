d19afd0fb4ab72c1e67d4bd83387844a0cd4d106
mvgaile <147670891+mvgaile@users.noreply.github.com>
2023-12-01 15:56:08 -0500

    Fix bug in constants.py

--- FoundIt/Backend/constants.py
+++ FoundIt/Backend/constants.py
@@ -12,4 +12,4 @@ EXTENSIONS = ["png", "gif", "jpg", "jpeg"]
 BASE_DIR = os.getcwd()
 S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com"
-PASSWORD = "4AAQSkZJRgABAQAAAQABAAD"
+PASSWORD = os.environ.get("PASSWORD")
